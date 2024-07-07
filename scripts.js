bbb25aa60d8ec11d63cae1b15beaf3bead85e85a
Chyogeesh <108213999+Chyogeesh@users.noreply.github.com>
2024-07-07 13:37:08 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function searchColleges() {
+    const query = document.getElementById('search').value;
+    alert(`Searching for colleges related to: ${query}`);
+    // Implement actual search functionality here
+}
